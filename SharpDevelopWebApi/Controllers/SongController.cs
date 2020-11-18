45236083211aa5233293591e22425477ab07d369
jielmar <72969467+jielmar@users.noreply.github.com>
2020-11-18 09:33:51 -0800

    added again a sharp

---
+++ SharpDevelopWebApi/Controllers/SongController.cs
@@ -0,0 +1,54 @@
+﻿
+using SharpDevelopWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+	
+    public class SongController : ApiController
+    {
+        [Route("api/song/getsong")]
+        [HttpGet]
+        public IHttpActionResult GeySong()
+        {
+
+            var mySong = new Song();
+            mySong.Id = 1;
+            mySong.Title = "hello my love";
+            mySong.Artist = "westlife";
+            mySong.Genre = "pop";
+            
+
+            return BadRequest("eror");
+        }
+    
+        [Route("api/song/")]
+        [HttpGet]
+        public IHttpActionResult GetSongs()
+        {
+            var songs = new List<Song>();
+
+            var song1 = new Song();
+            song1.Id = 1;
+            song1.Title = "welcome to my paradise";
+            song1.Artist = "bob marley";
+            song1.Genre = "slow rock";
+            songs.Add(song1);
+
+            var mySong = new Song();
+            mySong.Id = 1;
+            mySong.Title = "hello my love";
+            mySong.Artist = "westlife";
+            mySong.Genre = "pop";
+            songs.Add(mySong);
+
+           
+
+            return Ok(songs);
+        }
+
+    }
---
+++ SharpDevelopWebApi/Models/Song.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: janjie
+ * Date: 16/11/2020
+ * Time: 3:14 pm
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SharpDevelopWebApi.Models
+{
+	/// <summary>
+	/// Description of Song.
+	/// </summary>
+	public class Song
+	{
+		public int Id { get; set; }
+        public String Title { get; set; }
+        public string Artist { get; set; }
+        public String Genre{ get; set; }
+	}
+}
