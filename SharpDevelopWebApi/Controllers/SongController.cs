b6ad9d861ba7a17ae5bf9a9860edd78199c6dc2e
jielmar <72969467+jielmar@users.noreply.github.com>
2020-11-24 23:22:51 -0800

    transfer

---
+++ SharpDevelopWebApi/Controllers/SongController.cs
@@ -0,0 +1,102 @@
+﻿
+using SharpDevelopWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+	
+    public class SongController : ApiController
+    {
+    	SDWebApiDbContext _db  = new SDWebApiDbContext();
+    	
+    	[HttpGet]
+    	public IHttpActionResult GetAll(string search = "", string Artist = "", int ? year = null, int ? peak = null)
+    	{
+    		
+    		List<Song> songs;
+    		
+    		if(string.IsNullOrWhiteSpace(search))
+    		{
+    			
+    			songs =_db.Songs.ToList();
+    		}
+    	    else
+    		{
+    			songs = _db.Songs.Where( x => 
+    			                        x.Title.ToLower().Contains(search.ToLower())
+    			                        || x.Artist.ToLower().Contains(search.ToLower())
+    			                       ).OrderBy(or => or.Title).ToList();
+    			
+    		}
+    	     
+               if(!string.IsNullOrWhiteSpace(Artist))
+    	    {
+               	songs = songs.Where( x => x.Artist.ToLower().Contains( Artist.ToLower())).ToList();
+    	    }
+                 if ( year != null)
+    	    {
+    	    	songs=  songs.Where(x => x.YearRelease ==year.Value).ToList();
+    	    }
+    	     if( peak != null )
+    	    {
+    	    	songs=  songs.Where(x => x.peakChartposition <= peak).ToList();
+
+    	    }
+    	     int totalcount = songs.Count();
+    	     return Ok(new{totalcount, songs});
+    	}
+    	
+    	[HttpGet]
+    	public IHttpActionResult Get (int Id){
+    		
+    		var song  = _db.Songs.Find(Id);
+    		if (song != null){
+    			return Ok(song);
+    		}
+    		else{
+    			return BadRequest("Song not found");
+    		}
+    	}
+    	[HttpPost]
+    	public IHttpActionResult Create([FromBody]Song song){
+    		_db.Songs.Add(song);
+    		_db.SaveChanges();
+    		return Ok(song.Id);
+    	}
+    	[HttpPut]
+    	public IHttpActionResult Update([FromBody]Song updatedsong){
+    		var song = _db.Songs.Find(updatedsong.Id);
+    		if ( song != null){
+    			song.Artist = updatedsong.Artist;
+    		song.Title = updatedsong.Title;
+    		song.Genre = updatedsong.Genre;
+    		_db.Entry (song).State = System.Data.Entity.EntityState.Modified;	
+    		_db.SaveChanges();
+    		
+    		return Ok(song);
+    		}
+    		else{
+    			return BadRequest("error");
+    		}
+    	}
+    	[HttpDelete]
+        public IHttpActionResult Delete(int Id)
+        {
+            var song = _db.Songs.Find(Id);
+            if (song != null)
+            {
+                _db.Songs.Remove(song);
+                _db.SaveChanges();
+                return Ok("Song successfully deleted");
+            }
+            else{
+               return BadRequest("songs not found");
+            }
+              
+        }
+        
+    }
---
+++ SharpDevelopWebApi/Models/Song.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: janjie
+ * Date: 16/11/2020
+ * Time: 3:14 pm
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SharpDevelopWebApi.Models
+{
+	/// <summary>
+	/// Description of Song.
+	/// </summary>
+	public class Song
+	{
+		public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre{ get; set; }
+        public int peakChartposition { get; set; }
+        public int YearRelease { get; set; }
+	
+	}
+}
